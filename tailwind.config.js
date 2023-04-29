b7bdc79b7e44bd6da592e576e0a5d5c1a55325a7
oliveiramiro2 <ramiromares95@gmail.com>
2023-04-29 16:35:09 -0300

    added pallet of color's
    add project and config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,13 @@ module.exports = {
     content: ['./App.{js,jsx,ts,tsx}', './src/**/*.{js,jsx,ts,tsx}'],
     theme: {
         extend: {
+            colors: {
+                'plt-blue': '#0A71B6',
+                'plt-yellow': '#F9C40A',
+                'plt-black': '#190506',
+                'plt-orange': '#EB5432',
+                'plt-white': '#EAF2F0',
+            },
         },
     },
     plugins: [],
