2dfa1879652c850cbc6efdb6d867687164ad124f
oliveiramiro2 <ramiromares95@gmail.com>
2023-04-29 17:17:11 -0300

    added a teste default font of app
    add page

--- App.tsx
+++ App.tsx
@@ -1,10 +1,35 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import {
+    useFonts,
+    Rufina_400Regular,
+    Rufina_700Bold,
+} from '@expo-google-fonts/rufina';
 
-const App: React.FC = () => (
-    <View>
-        <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
-);
+const App: React.FC = () => {
+    const [fontsLoaded] = useFonts({
+        Rufina_400Regular,
+        Rufina_700Bold,
+    });
+
+    if (!fontsLoaded) {
+        return (
+            <View className="bg-plt-black flex-1 items-center justify-center">
+                <Text className="text-plt-orange">carregando...</Text>
+            </View>
+        );
+    }
+
+    return (
+        <View className="bg-plt-black flex-1 items-center justify-center">
+            <Text className="text-plt-orange font-bold">
+                Open up App.tsx to start working on your app!
+            </Text>
+            <Text className="text-plt-orange font-dflt_regular">
+                Open up App.tsx to start working on your app!
+            </Text>
+        </View>
+    );
+};
 
 export default App;
