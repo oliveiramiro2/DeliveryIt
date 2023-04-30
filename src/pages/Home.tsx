4253cf73e21ae412d95297f3e1c2f550020a441a
oliveiramiro2 <ramiromares95@gmail.com>
2023-04-30 15:10:45 -0300

    added button to take user till the promotions
    add component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Image, Text, View, TouchableOpacity } from 'react-native';
 
 import { ContainPages } from '../components';
+import promotion from '../assets/imgs/off.png';
 
 export const Home: React.FC = () => (
     <ContainPages>
@@ -9,6 +10,12 @@ export const Home: React.FC = () => (
             <Text className="text-plt-yellow font-dflt-bold text-2xl">
                 Bem vindo ao Delivery It!
             </Text>
+            <TouchableOpacity className="bg-plt-white w-full h-[48vh] mt-10 rounded-lg flex items-center justify-center border-2 border-plt-yellow">
+                <Text className="text-plt-orange font-dflt-regular text-2xl mb-3">
+                    Promoções
+                </Text>
+                <Image source={promotion} resizeMode="contain" className="w-[95%] h-[40vh] rounded-md" />
+            </TouchableOpacity>
         </View>
     </ContainPages>
 );
