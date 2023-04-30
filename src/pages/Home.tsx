24e8af308022f2378bf4a37ae3f4b7dee4f60c05
oliveiramiro2 <ramiromares95@gmail.com>
2023-04-30 15:38:31 -0300

    added animate in title and button promotinos
    add component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Image, Text, View, TouchableOpacity } from 'react-native';
+import { Image, Text, View, Pressable } from 'react-native';
+import { MotiView, MotiText } from 'moti';
 
 import { ContainPages } from '../components';
 import promotion from '../assets/imgs/off.png';
@@ -7,15 +8,29 @@ import promotion from '../assets/imgs/off.png';
 export const Home: React.FC = () => (
     <ContainPages>
         <View className="flex items-center">
-            <Text className="text-plt-yellow font-dflt-bold text-2xl">
+            <MotiText
+                from={{ bottom: 50, opacity: 0, scaleY: 0 }}
+                animate={{ bottom: 0, opacity: 1, scaleY: 1.5 }}
+                className="text-plt-yellow font-dflt-bold text-2xl"
+            >
                 Bem vindo ao Delivery It!
-            </Text>
-            <TouchableOpacity className="bg-plt-white w-full h-[48vh] mt-10 rounded-lg flex items-center justify-center border-2 border-plt-yellow">
-                <Text className="text-plt-orange font-dflt-regular text-2xl mb-3">
-                    Promoções
-                </Text>
-                <Image source={promotion} resizeMode="contain" className="w-[95%] h-[40vh] rounded-md" />
-            </TouchableOpacity>
+            </MotiText>
+            <MotiView
+                className="w-full"
+                from={{ opacity: 0, rotateY: '180deg' }}
+                animate={{ opacity: 1, rotateY: '0deg' }}
+            >
+                <Pressable className="bg-plt-white w-full h-[48vh] mt-7 rounded-lg flex items-center justify-center border-2 border-plt-yellow">
+                    <Text className="text-plt-orange font-dflt-regular text-2xl mb-3">
+                        Promoções
+                    </Text>
+                    <Image
+                        source={promotion}
+                        resizeMode="contain"
+                        className="w-[95%] h-[40vh] rounded-md"
+                    />
+                </Pressable>
+            </MotiView>
         </View>
     </ContainPages>
 );
