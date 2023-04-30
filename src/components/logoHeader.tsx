61757e7434134003b78994f9c695bd617af2c214
oliveiramiro2 <ramiromares95@gmail.com>
2023-04-30 09:28:46 -0300

    added a logic to animation work
    add component

--- src/components/logoHeader.tsx
+++ src/components/logoHeader.tsx
@@ -3,20 +3,25 @@ import { View } from 'react-native';
 import { MotiImage } from 'moti';
 
 import logo from '../assets/imgs/logo.png';
+import { useAnimation } from './hooks';
 
-export const LogoHeader: React.FC = () => (
-    <View className="pr-2">
-        <MotiImage
-            source={logo}
-            className="h-16 w-16"
-            resizeMode="contain"
-            from={{ opacity: 0, scale: 0 }}
-            animate={{ opacity: 1, scale: 1 }}
-            transition={{
-                delay: 300,
-                type: 'timing',
-                duration: 650,
-            }}
-        />
-    </View>
-);
+export const LogoHeader: React.FC = () => {
+    const { handleAnimation } = useAnimation();
+
+    return (
+        <View className="pr-2">
+            <MotiImage
+                source={logo}
+                className="h-16 w-16"
+                resizeMode="contain"
+                from={{ opacity: 0, scale: 0, backgroundColor: '#000' }}
+                animate={handleAnimation}
+                transition={{
+                    delay: 300,
+                    type: 'timing',
+                    duration: 500,
+                }}
+            />
+        </View>
+    );
+};
