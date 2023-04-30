1462ad8c8c3b94edfeeb36b11e79af76fdcc6a36
oliveiramiro2 <ramiromares95@gmail.com>
2023-04-30 15:09:48 -0300

    added padding top of header
    add component

--- src/components/containPages.tsx
+++ src/components/containPages.tsx
@@ -4,7 +4,7 @@ import { ScrollView, View } from 'react-native';
 export const ContainPages: React.FC<{ children: React.ReactNode }> = ({
     children,
 }) => (
-    <View className="bg-black flex-1 pl-2 pr-2 pt-1">
+    <View className="bg-black flex-1 pl-2 pr-2 pt-6">
         <ScrollView
             showsHorizontalScrollIndicator={false}
             showsVerticalScrollIndicator={false}
