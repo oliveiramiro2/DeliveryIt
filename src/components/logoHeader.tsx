a047aeaa9a7756af2d57751e92ce80792599b072
oliveiramiro2 <ramiromares95@gmail.com>
2023-04-30 08:45:24 -0300

    added the logo img component
    add component

--- src/components/logoHeader.tsx
+++ src/components/logoHeader.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import { MotiImage } from 'moti';
+
+import logo from '../assets/imgs/logo.png';
 
 export const LogoHeader: React.FC = () => (
-    <View>
-        <Text>logoHeader</Text>
+    <View className="pr-2">
+        <MotiImage
+            source={logo}
+            className="h-16 w-16"
+            resizeMode="contain"
+            from={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+                delay: 300,
+                type: 'timing',
+                duration: 650,
+            }}
+        />
     </View>
 );
