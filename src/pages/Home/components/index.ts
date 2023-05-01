498f2d9ccf806c9f2abde706e65bdea37c14cd71
oliveiramiro2 <ramiromares95@gmail.com>
2023-05-01 10:30:17 -0300

    added register component to show into home when user is not logined
    add component

---
+++ src/pages/Home/components/index.ts
@@ -0,0 +1,1 @@
+export { RegisterHome } from './register/register';
---
+++ src/pages/Home/components/register/register.tsx
@@ -0,0 +1,8 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+export const RegisterHome: React.FC = () => (
+    <View>
+        <Text>RegisterHome</Text>
+    </View>
+);
