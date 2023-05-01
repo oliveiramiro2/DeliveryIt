2cdeeca03eef300e2b9f46cc332e8ee4778f5407
oliveiramiro2 <ramiromares95@gmail.com>
2023-05-01 13:58:42 -0300

    added layout of login home component
    add component

--- src/pages/Home/components/login/login.tsx
+++ src/pages/Home/components/login/login.tsx
@@ -1,8 +1,15 @@
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 import React from 'react';
 
 export const LoginHome: React.FC = () => (
-    <View>
-        <Text>login</Text>
+    <View className="my-5">
+        <Text className="text-plt-white text-center mb-2 font-dflt-regular text-2xl">
+            Já possui cadastro!!
+        </Text>
+        <TouchableOpacity className="bg-plt-blue py-2 items-center rounded-lg mt-3">
+            <Text className="text-plt-white font-dflt-regular text-lg">
+                Faça seu login
+            </Text>
+        </TouchableOpacity>
     </View>
 );
