e85e5940f20cdd3e359562e26fe33760bbfccd79
oliveiramiro2 <ramiromares95@gmail.com>
2023-05-01 09:56:26 -0300

    adjusted type of image
    adjust types

--- src/@types/categories.d.ts
+++ src/@types/categories.d.ts
@@ -1,6 +1,6 @@
-/* global ImageBitmapSource */
+/* global ImageSourcePropType */
 export interface ICategoriesData {
     id: number;
     name: 'Açougue' | 'Bebidas' | 'Doces' | 'Frios' | 'Hortifruti' | 'Lanches' | 'Limpeza' | 'Padaria';
-    image: ImageBitmapSource;
+    image: ImageSourcePropType;
 }
