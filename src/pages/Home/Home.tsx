ce7e985ec472e0658078f9ebb2ddb465c55bd542
oliveiramiro2 <ramiromares95@gmail.com>
2023-05-01 10:46:33 -0300

    added component register when user is not logined
    add page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
-import { Image, Text, View, Pressable, FlatList } from 'react-native';
+import {
+    Image,
+    Text,
+    View,
+    Pressable,
+    FlatList,
+} from 'react-native';
 import { MotiView } from 'moti';
 
 import { CaregoriesItem, ContainPages } from '../../components';
 import promotion from '../../assets/imgs/off.png';
 import { categoriesData } from '../../constants';
+import { RegisterHome } from './components';
 
 export const Home: React.FC = () => (
     <ContainPages>
@@ -13,7 +20,7 @@ export const Home: React.FC = () => (
                 Bem vindo ao Delivery It!
             </Text>
             <View className="bg-plt-black w-full mt-5">
-                <Text className="text-plt-white font-dflt-regular text-2xl mb-3">
+                <Text className="text-plt-white font-dflt-bold text-2xl mb-3">
                     Principais categorias
                 </Text>
                 <FlatList
@@ -36,7 +43,7 @@ export const Home: React.FC = () => (
                 animate={{ opacity: 1, rotateY: '0deg' }}
             >
                 <Pressable className="bg-plt-white w-full h-[48vh] mt-7 rounded-lg flex items-center justify-center border-2 border-plt-yellow">
-                    <Text className="text-plt-orange font-dflt-regular text-2xl mb-3">
+                    <Text className="text-plt-orange font-dflt-bold text-2xl mb-3">
                         Promoções
                     </Text>
                     <Image
@@ -46,6 +53,7 @@ export const Home: React.FC = () => (
                     />
                 </Pressable>
             </MotiView>
+            <RegisterHome />
         </View>
     </ContainPages>
 );
