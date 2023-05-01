6f82d317b6f0b51caa9b9bf8aba1ff63bcaa10d6
oliveiramiro2 <ramiromares95@gmail.com>
2023-05-01 14:55:16 -0300

    added a export of git to show when loading font
    add constant

--- src/constants/images.ts
+++ src/constants/images.ts
@@ -1,3 +1,5 @@
 import promotionIMG from '../assets/imgs/off.png';
+import deliveryIMG from '../assets/imgs/delivery.gif';
 
 export const promotion = promotionIMG;
+export const delivery = deliveryIMG;
