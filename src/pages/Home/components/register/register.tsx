b92709e93f0d5aa202855ccb9fdb2e2cca9b9505
oliveiramiro2 <ramiromares95@gmail.com>
2023-05-01 10:46:05 -0300

    added layout of register home component
    add component

--- src/pages/Home/components/register/register.tsx
+++ src/pages/Home/components/register/register.tsx
@@ -1,8 +1,22 @@
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 import React from 'react';
 
 export const RegisterHome: React.FC = () => (
-    <View>
-        <Text>RegisterHome</Text>
+    <View className="my-5">
+        <Text className="text-plt-white text-center mb-2 font-dflt-regular text-2xl">
+            Faça seu cadastro!!
+        </Text>
+        <Text className="text-plt-white font-dflt-regular text-lg mb-2">
+            Como consumido compre e receba do conforto da sua casa!
+        </Text>
+        <Text className="text-plt-white font-dflt-regular text-lg">
+            Como vendedor acelere as vendas do seu Super Mercado com vendas
+            online, e mostro para seus clientes os produtos direto no celular!
+        </Text>
+        <TouchableOpacity className="bg-plt-blue py-2 items-center rounded-lg mt-3">
+            <Text className="text-plt-white font-dflt-regular text-lg">
+                Cadastrar
+            </Text>
+        </TouchableOpacity>
     </View>
 );
