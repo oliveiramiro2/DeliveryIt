d4ee88f204ab579c9ad122b67d7845b0ff67e171
oliveiramiro2 <ramiromares95@gmail.com>
2023-05-01 15:07:21 -0300

    added loading into app
    add app

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import { Text, View, StatusBar } from 'react-native';
+import { StatusBar } from 'react-native';
 import {
     useFonts,
     Rufina_400Regular,
     Rufina_700Bold,
 } from '@expo-google-fonts/rufina';
+
 import RoutesDeliveryIt from './src/routes';
+import { LoadingApp } from './src/components/index';
 
 const App: React.FC = () => {
     const [fontsLoaded] = useFonts({
@@ -13,13 +15,7 @@ const App: React.FC = () => {
         Rufina_700Bold,
     });
 
-    if (!fontsLoaded) {
-        return (
-            <View className="bg-black flex-1 items-center justify-center">
-                <Text className="text-plt-orange">carregando...</Text>
-            </View>
-        );
-    }
+    if (!fontsLoaded) return <LoadingApp />;
 
     return (
         <>
