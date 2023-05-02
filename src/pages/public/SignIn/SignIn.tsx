83ac3d0fea673d68bd77d02c6e869192bb866004
oliveiramiro2 <ramiromares95@gmail.com>
2023-05-02 09:10:32 -0300

    added the title and icon into sign in
    add page

--- src/pages/public/SignIn/SignIn.tsx
+++ src/pages/public/SignIn/SignIn.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import { FontAwesome } from '@expo/vector-icons';
+
+import { ContainPages } from '../../../components';
 
 export const SignIn: React.FC = () => (
-    <View>
-        <Text>SignIn</Text>
-    </View>
+    <ContainPages>
+        <View className="flex items-center">
+            <Text className="text-plt-yellow font-dflt-bold text-2xl">
+                Faça seu login!
+            </Text>
+
+            <View className="mt-10">
+                <FontAwesome name="user-circle-o" color="#fff" size={80} />
+            </View>
+        </View>
+    </ContainPages>
 );
