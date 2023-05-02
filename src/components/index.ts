6f2cc1308ead32d9ff03bfe7444f3d704967cc86
oliveiramiro2 <ramiromares95@gmail.com>
2023-05-02 09:23:58 -0300

    added default to inputs
    add component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { LogoHeader } from './logoHeader/logoHeader';
 export { ContainPages } from './containPage/containPages';
 export { LoadingApp } from './loadingApp/loadingApp';
+export { Input } from './input/input';
---
+++ src/components/input/input.tsx
@@ -0,0 +1,8 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+export const Input: React.FC = () => (
+    <View>
+        <Text>input</Text>
+    </View>
+);
