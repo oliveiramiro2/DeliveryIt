ba1fabffc9701151e3af60b2f8b740c68ca69ede
oliveiramiro2 <ramiromares95@gmail.com>
2023-05-03 14:52:36 -0300

    added title of page
    add page

--- src/pages/public/SignUp/SignUp.tsx
+++ src/pages/public/SignUp/SignUp.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
+import { ContainPages } from '../../../components';
+
 export const SignUp: React.FC = () => (
-    <View>
-        <Text>SignUp</Text>
-    </View>
+    <ContainPages>
+        <View style={{ flex: 1 }} className="items-center min-h-[80vh]">
+            <Text className="text-plt-yellow font-dflt-bold text-2xl">
+                Faça seu cadastro!
+            </Text>
+        </View>
+    </ContainPages>
 );
