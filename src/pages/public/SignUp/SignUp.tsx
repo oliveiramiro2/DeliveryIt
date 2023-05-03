4c9b5470a7cd7feee59ee801b28cf8cda99bbfaf
oliveiramiro2 <ramiromares95@gmail.com>
2023-05-03 19:44:17 -0300

    added navigate between stack pages
    add pages

--- src/pages/public/SignUp/SignUp.tsx
+++ src/pages/public/SignUp/SignUp.tsx
@@ -2,54 +2,72 @@ import { FontAwesome, Ionicons } from '@expo/vector-icons';
 import React from 'react';
 import { View, Text, Pressable } from 'react-native';
 import { MotiView } from 'moti';
-
+import { NavigationProp, useNavigation } from '@react-navigation/native';
 import { ContainPages } from '../../../components';
 
-export const SignUp: React.FC = () => (
-    <ContainPages>
-        <View className="items-center">
-            <Text className="text-plt-yellow font-dflt-bold text-2xl">
-                Faça seu cadastro!
-            </Text>
+import { IStackSignUp } from '../../../routes/public/@types';
+
+export const SignUp: React.FC = () => {
+    const navigationStack = useNavigation<NavigationProp<IStackSignUp>>();
+
+    return (
+        <ContainPages>
             <View className="items-center">
-                <View className="flex flex-row items-center justify-center gap-x-5 mt-[25vh]">
-                    <MotiView
-                        from={{ opacity: 0 }}
-                        animate={{ opacity: 1 }}
-                        className="w-[40vw]"
-                    >
-                        <Pressable className="items-center justify-center w-[40vw] bg-plt-blue py-2 rounded-lg">
-                            <Text className="text-plt-white max-w-[40vw] text-center text-lg mb-2 font-dflt-regular">
-                                Cadastro de consumidor!
-                            </Text>
-                            <FontAwesome name="user" color="#fff" size={40} />
-                        </Pressable>
-                    </MotiView>
-                    <MotiView
-                        from={{ opacity: 0 }}
-                        animate={{ opacity: 1 }}
-                        className="w-[40vw]"
-                    >
-                        <Pressable
-                            style={{
-                                shadowOffset: { width: 5000, height: 5000 },
-                                shadowColor: '#fff',
-                                shadowOpacity: 100,
-                            }}
-                            className="items-center justify-center w-[40vw] bg-plt-blue py-2 rounded-lg"
+                <Text className="text-plt-yellow font-dflt-bold text-2xl">
+                    Faça seu cadastro!
+                </Text>
+                <View className="items-center">
+                    <View className="flex flex-row items-center justify-center gap-x-5 mt-[25vh]">
+                        <MotiView
+                            from={{ opacity: 0 }}
+                            animate={{ opacity: 1 }}
+                            className="w-[40vw]"
                         >
-                            <Text className="text-plt-white max-w-[40vw] text-center text-lg mb-2 font-dflt-regular">
-                                Cadastro de empresa!
-                            </Text>
-                            <Ionicons
-                                name="business-sharp"
-                                color="#fff"
-                                size={40}
-                            />
-                        </Pressable>
-                    </MotiView>
+                            <Pressable
+                                onPress={() => {
+                                    navigationStack.navigate('ConsumerStack');
+                                }}
+                                className="items-center justify-center w-[40vw] bg-plt-blue py-2 rounded-lg"
+                            >
+                                <Text className="text-plt-white max-w-[40vw] text-center text-lg mb-2 font-dflt-regular">
+                                    Cadastro de consumidor!
+                                </Text>
+                                <FontAwesome
+                                    name="user"
+                                    color="#fff"
+                                    size={40}
+                                />
+                            </Pressable>
+                        </MotiView>
+                        <MotiView
+                            from={{ opacity: 0 }}
+                            animate={{ opacity: 1 }}
+                            className="w-[40vw]"
+                        >
+                            <Pressable
+                                style={{
+                                    shadowOffset: { width: 5000, height: 5000 },
+                                    shadowColor: '#fff',
+                                    shadowOpacity: 100,
+                                }}
+                                className="items-center justify-center w-[40vw] bg-plt-blue py-2 rounded-lg"
+                                onPress={() => {
+                                    navigationStack.navigate('BusinessStack');
+                                }}
+                            >
+                                <Text className="text-plt-white max-w-[40vw] text-center text-lg mb-2 font-dflt-regular">
+                                    Cadastro de empresa!
+                                </Text>
+                                <Ionicons
+                                    name="business-sharp"
+                                    color="#fff"
+                                    size={40}
+                                />
+                            </Pressable>
+                        </MotiView>
+                    </View>
                 </View>
             </View>
-        </View>
-    </ContainPages>
-);
+        </ContainPages>
+    );
+};
