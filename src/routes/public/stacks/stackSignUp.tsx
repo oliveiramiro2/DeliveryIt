cb0b7aac1899f711cd43ce4886f63c53d025d1ed
oliveiramiro2 <ramiromares95@gmail.com>
2023-05-03 18:53:42 -0300

    put page into stacks function
    add route stack

--- src/routes/public/stacks/stackSignUp.tsx
+++ src/routes/public/stacks/stackSignUp.tsx
@@ -1,5 +1,6 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { SignUp } from '../../../pages/public';
+import { Business, Consumer } from '../../../pages/public/SignUp/components';
 import { IStackSignUp } from '../@types';
 
 const Stack = createNativeStackNavigator<IStackSignUp>();
@@ -7,7 +8,7 @@ const Stack = createNativeStackNavigator<IStackSignUp>();
 export const StackSignUp = () => (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
         <Stack.Screen name="SignUpStack" component={SignUp} />
-        <Stack.Screen name="ConsumerStack" component={} />
-        <Stack.Screen name="BusinessStack" component={} />
+        <Stack.Screen name="ConsumerStack" component={Consumer} />
+        <Stack.Screen name="BusinessStack" component={Business} />
     </Stack.Navigator>
 );
