8f3d81f9a8bda230d91ec2de9b9b6ef456db9d62
oliveiramiro2 <ramiromares95@gmail.com>
2023-05-03 16:28:32 -0300

    added animation in elements of layout
    add page

--- src/pages/public/SignUp/SignUp.tsx
+++ src/pages/public/SignUp/SignUp.tsx
@@ -1,6 +1,7 @@
 import { FontAwesome, Ionicons } from '@expo/vector-icons';
 import React from 'react';
 import { View, Text, Pressable } from 'react-native';
+import { MotiView } from 'moti';
 
 import { ContainPages } from '../../../components';
 
@@ -12,25 +13,41 @@ export const SignUp: React.FC = () => (
             </Text>
             <View className="items-center">
                 <View className="flex flex-row items-center justify-center gap-x-5 mt-[25vh]">
-                    <Pressable className="items-center justify-center w-[40vw] bg-plt-blue py-2 rounded-lg">
-                        <Text className="text-plt-white max-w-[40vw] text-center text-lg mb-2 font-dflt-regular">
-                            Cadastro de consumidor!
-                        </Text>
-                        <FontAwesome name="user" color="#fff" size={40} />
-                    </Pressable>
-                    <Pressable
-                        style={{ shadowOffset: { width: 5000, height: 5000 }, shadowColor: '#fff', shadowOpacity: 100 }}
-                        className="items-center justify-center w-[40vw] bg-plt-blue py-2 rounded-lg"
+                    <MotiView
+                        from={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        className="w-[40vw]"
                     >
-                        <Text className="text-plt-white max-w-[40vw] text-center text-lg mb-2 font-dflt-regular">
-                            Cadastro de empresa!
-                        </Text>
-                        <Ionicons
-                            name="business-sharp"
-                            color="#fff"
-                            size={40}
-                        />
-                    </Pressable>
+                        <Pressable className="items-center justify-center w-[40vw] bg-plt-blue py-2 rounded-lg">
+                            <Text className="text-plt-white max-w-[40vw] text-center text-lg mb-2 font-dflt-regular">
+                                Cadastro de consumidor!
+                            </Text>
+                            <FontAwesome name="user" color="#fff" size={40} />
+                        </Pressable>
+                    </MotiView>
+                    <MotiView
+                        from={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        className="w-[40vw]"
+                    >
+                        <Pressable
+                            style={{
+                                shadowOffset: { width: 5000, height: 5000 },
+                                shadowColor: '#fff',
+                                shadowOpacity: 100,
+                            }}
+                            className="items-center justify-center w-[40vw] bg-plt-blue py-2 rounded-lg"
+                        >
+                            <Text className="text-plt-white max-w-[40vw] text-center text-lg mb-2 font-dflt-regular">
+                                Cadastro de empresa!
+                            </Text>
+                            <Ionicons
+                                name="business-sharp"
+                                color="#fff"
+                                size={40}
+                            />
+                        </Pressable>
+                    </MotiView>
                 </View>
             </View>
         </View>
