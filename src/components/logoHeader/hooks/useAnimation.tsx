6752180560e45b6db930cc2e5b1d6b5c18bf4a6c
oliveiramiro2 <ramiromares95@gmail.com>
2023-05-04 11:37:39 -0300

    added the extra time when enter into a stack
    add hook

--- src/components/logoHeader/hooks/useAnimation.tsx
+++ src/components/logoHeader/hooks/useAnimation.tsx
@@ -2,17 +2,17 @@ import { useMemo, useState } from 'react';
 
 import { IAnimationLogo } from '../@types';
 
-export const useAnimation = () => {
+export const useAnimation = (time: number) => {
     const [animation, setAnimation] = useState<IAnimationLogo>({
         opacity: 0.5,
         scale: 1.5,
-        backgroundColor: '#0000000.5',
+        backgroundColor: '#ffffff61',
     });
 
     const handleAnimation = useMemo(() => {
         setTimeout(() => {
             setAnimation({ opacity: 1, scale: 1, backgroundColor: 'none' });
-        }, 800);
+        }, 800 + time);
         return animation;
     }, [animation]);
 
