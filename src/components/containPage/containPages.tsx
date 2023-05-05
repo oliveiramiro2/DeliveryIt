93e8e143e6ced529f1d0cf01ebf867dee599b139
oliveiramiro2 <ramiromares95@gmail.com>
2023-05-05 10:07:43 -0300

    added a flex 1 to page get all screen
    add page

--- src/components/containPage/containPages.tsx
+++ src/components/containPage/containPages.tsx
@@ -4,10 +4,11 @@ import { ScrollView, View } from 'react-native';
 export const ContainPages: React.FC<{ children: React.ReactNode }> = ({
     children,
 }) => (
-    <View className="bg-black flex-1 pl-2 pr-2 pt-6">
+    <View style={{ flex: 1 }} className="bg-black flex-1 pl-2 pr-2 pt-6">
         <ScrollView
             showsHorizontalScrollIndicator={false}
             showsVerticalScrollIndicator={false}
+            style={{ flex: 1 }}
         >
             {children}
         </ScrollView>
