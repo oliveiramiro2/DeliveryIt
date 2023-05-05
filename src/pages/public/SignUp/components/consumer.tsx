daf62e9b0859b308a0693f16554be0ab647cc67f
oliveiramiro2 <ramiromares95@gmail.com>
2023-05-05 10:08:37 -0300

    added the title and the min size of page
    add page stack

--- src/pages/public/SignUp/components/consumer.tsx
+++ src/pages/public/SignUp/components/consumer.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
+import { ContainPages } from '../../../../components';
+
 export const Consumer: React.FC = () => (
-    <View>
-        <Text>vonsumer</Text>
-    </View>
+    <ContainPages>
+        <View className="items-center min-h-[82vh]">
+            <Text className="text-plt-yellow font-dflt-bold text-2xl">
+                Cadastre-se para comprar!
+            </Text>
+        </View>
+    </ContainPages>
 );
