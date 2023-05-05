1902f477f3326731f37063a58a65cfa3c621898c
oliveiramiro2 <ramiromares95@gmail.com>
2023-05-05 10:08:33 -0300

    added the title and the min size of page
    add page stack

--- src/pages/public/SignUp/components/business.tsx
+++ src/pages/public/SignUp/components/business.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
+import { ContainPages } from '../../../../components';
+
 export const Business: React.FC = () => (
-    <View>
-        <Text>business</Text>
-    </View>
+    <ContainPages>
+        <View style={{ flex: 1 }} className="items-center min-h-[82vh]">
+            <Text className="text-plt-yellow font-dflt-bold text-2xl">
+                Cadastre sua empresa!
+            </Text>
+        </View>
+    </ContainPages>
 );
