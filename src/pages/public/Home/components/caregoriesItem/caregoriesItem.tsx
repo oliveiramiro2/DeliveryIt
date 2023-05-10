4bb07085a981cb8c7d0c3499cf2d5cd5b5d8c1a9
oliveiramiro2 <ramiromares95@gmail.com>
2023-05-10 20:25:01 -0300

    added navigation to sing in
    add component categories items

--- src/pages/public/Home/components/caregoriesItem/caregoriesItem.tsx
+++ src/pages/public/Home/components/caregoriesItem/caregoriesItem.tsx
@@ -1,25 +1,37 @@
 import React from 'react';
 import { MotiPressable } from 'moti/interactions';
 import { Text, Image } from 'react-native';
+import { NavigationProp, useNavigation } from '@react-navigation/native';
 
 import { ICategoriesData } from '../../../../../@types';
+import { ITabPublicNavigator } from '../../../../../routes/public/@types';
 
 export const CaregoriesItem: React.FC<{ data: ICategoriesData }> = ({
     data,
-}) => (
-    <MotiPressable
-        from={{ opacity: 0, scaleY: 0 }}
-        animate={{ opacity: 1, scaleY: 1 }}
-        transition={{
-            delay: 300 * data.id,
-            type: 'timing',
-            duration: 300,
-        }}
-        style={{ marginRight: 20, alignItems: 'center' }}
-    >
-        <Image source={data.image} resizeMode="contain" className="w-20 h-24" />
-        <Text className="text-plt-white font-dflt-regular text">
-            {data.name}
-        </Text>
-    </MotiPressable>
-);
+}) => {
+    const navigationStack =
+        useNavigation<NavigationProp<ITabPublicNavigator>>();
+
+    return (
+        <MotiPressable
+            from={{ opacity: 0, scaleY: 0 }}
+            animate={{ opacity: 1, scaleY: 1 }}
+            transition={{
+                delay: 300 * data.id,
+                type: 'timing',
+                duration: 300,
+            }}
+            style={{ marginRight: 20, alignItems: 'center' }}
+            onPress={() => navigationStack.navigate('SignInTab')}
+        >
+            <Image
+                source={data.image}
+                resizeMode="contain"
+                className="w-20 h-24"
+            />
+            <Text className="text-plt-white font-dflt-regular text">
+                {data.name}
+            </Text>
+        </MotiPressable>
+    );
+};
