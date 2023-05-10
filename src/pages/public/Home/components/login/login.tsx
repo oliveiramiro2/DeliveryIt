c96d5bd60cb8a38af6404d9b9def551d053a3f91
oliveiramiro2 <ramiromares95@gmail.com>
2023-05-10 20:25:10 -0300

    added navigation to sing in
    add component login

--- src/pages/public/Home/components/login/login.tsx
+++ src/pages/public/Home/components/login/login.tsx
@@ -1,15 +1,26 @@
-import { View, Text, TouchableOpacity } from 'react-native';
 import React from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
+import { NavigationProp, useNavigation } from '@react-navigation/native';
 
-export const LoginHome: React.FC = () => (
-    <View className="w-full mb-10 mt-5 pt-5 border-t border-plt-yellow">
-        <Text className="text-plt-white text-center mb-2 font-dflt-regular text-2xl">
-            Já possui cadastro!!
-        </Text>
-        <TouchableOpacity className="bg-plt-blue py-2 items-center rounded-lg mt-3">
-            <Text className="text-plt-white font-dflt-regular text-lg">
-                Faça seu login
+import { ITabPublicNavigator } from '../../../../../routes/public/@types';
+
+export const LoginHome: React.FC = () => {
+    const navigationStack =
+        useNavigation<NavigationProp<ITabPublicNavigator>>();
+
+    return (
+        <View className="w-full mb-10 mt-5 pt-5 border-t border-plt-yellow">
+            <Text className="text-plt-white text-center mb-2 font-dflt-regular text-2xl">
+                Já possui cadastro!!
             </Text>
-        </TouchableOpacity>
-    </View>
-);
+            <TouchableOpacity
+                className="bg-plt-blue py-2 items-center rounded-lg mt-3"
+                onPress={() => navigationStack.navigate('SignInTab')}
+            >
+                <Text className="text-plt-white font-dflt-regular text-lg">
+                    Faça seu login
+                </Text>
+            </TouchableOpacity>
+        </View>
+    );
+};
